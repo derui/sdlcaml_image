13e6437ee705881735489760a7e6da82462f9805
derui <derutakayu@gmail.com>
2016-04-26 22:32:06 +0900

    Add image loader module

---
+++ src/sdlcaml_image_load.ml
@@ -0,0 +1,48 @@
+open Ctypes
+open Foreign
+
+module S = Sdlcaml.Std
+module E = Sdlcaml_image_error
+
+type image_type = BMP | CUR | GIF | ICO | JPG | LBM | PCX | PNG | PNM | TGA | TIF | XGF | XPM | XV
+
+let rwops = ptr S.Structures.Rw_ops.t
+
+module Inner = struct
+  let load = foreign "IMG_Load" (string @-> returning Sdlcaml.Std.Types.Surface.t)
+  let load_rw = foreign "IMG_Load_RW" (rwops @-> bool @-> returning Sdlcaml.Std.Types.Surface.t)
+  let load_typed_rw = foreign "IMG_LoadTyped_RW" (rwops @-> bool @-> string @-> returning Sdlcaml.Std.Types.Surface.t)
+end
+
+let wrap pred result =
+  if pred () then S.Types.Result.return result
+  else S.Types.Result.Failure (E.get ())
+
+let load fname =
+  let ret = Inner.load fname in
+  wrap (fun () -> to_voidp ret <> null) ret
+
+let load_rw ?(free_src=true) src =
+  let ret = Inner.load_rw src free_src in
+  wrap (fun () -> to_voidp ret <> null) ret
+
+let string_of_image_type = function
+  | BMP -> "BMP"
+  | CUR -> "CUR"
+  | GIF -> "GIF"
+  | ICO -> "ICO"
+  | JPG -> "JPG"
+  | LBM -> "LBM"
+  | PCX -> "PCX"
+  | PNG -> "PNG"
+  | PNM -> "PNM"
+  | TGA -> "TGA"
+  | TIF -> "TIF"
+  | XGF -> "XGF"
+  | XPM -> "XPM"
+  | XV  -> "XV"
+
+let load_typed_rw ?(free_src=true) ~typ ~src =
+  let typ = string_of_image_type typ in
+  let ret = Inner.load_typed_rw src free_src typ in
+  wrap (fun () -> to_voidp ret <> null) ret
---
+++ src/sdlcaml_image_load.mli
@@ -0,0 +1,26 @@
+(**
+   Define a module to provide operations to get infomation from the RWops.
+
+   @author derui
+   @since 0.1
+*)
+
+type image_type = BMP | CUR | GIF | ICO | JPG | LBM | PCX | PNG | PNM | TGA | TIF | XGF | XPM | XV
+
+val load: string -> Sdlcaml.Std.Surface.t Sdlcaml.Std.Types.Result.t
+(* [load fname] Load [fname] for use as an image in a new surface. Don't forget to [Sdlcaml.Std.Surface.free] the returned
+   surface when you are through with it.
+*)
+
+val load_rw: ?free_src:bool -> Sdlcaml.Std.RWops.t -> Sdlcaml.Std.Surface.t Sdlcaml.Std.Types.Result.t
+(* [load_rw ?free_src src] Load [src] for use as a surface. Don't forget to [Sdlcaml.Std.Surface.free] the returned
+   surface when you are through with it.
+   If [free_src] not specified or specified for true, you don't have to need call [Sdlcaml.Std.RWops.close] to [src].
+*)
+
+val load_typed_rw: ?free_src:bool -> typ:image_type -> src:Sdlcaml.Std.RWops.t -> Sdlcaml.Std.Surface.t Sdlcaml.Std.Types.Result.t
+(* [load_rw ?free_src ~typ ~src] Load [src] for use as a surface. This function does not guarantee that the format specified
+   by [typ] is the format of the loaded image.
+   Don't forget to [Sdlcaml.Std.Surface.free] the returned surface when you are through with it.
+   If [free_src] not specified or specified for true, you don't have to need call [Sdlcaml.Std.RWops.close] to [src].
+*)
